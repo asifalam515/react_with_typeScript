719dc44fbc305b65766b39ed0de104cd395caea1
asifalam515 <asibulalam515@gmail.com>
2024-12-13 09:24:23 +0600

    started

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,13 @@
+const CourseGoal = (props) => {
+  return (
+    <article>
+      <div>
+        <h2 className="text-2xl">Title</h2>
+        <p>Description</p>
+      </div>
+      <button className="btn"> Delete</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
