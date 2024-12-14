c5a6d473b10d23a75b5c96a6d7dfffade990cd47
asifalam515 <asibulalam515@gmail.com>
2024-12-14 23:16:12 +0800

    updaed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,26 @@ type HeaderProps = {
   };
 };
 const Header = ({ image, children }: HeaderProps) => {
+  Swal.fire({
+    title: "<strong>HTML <u>example</u></strong>",
+    icon: "info",
+    html: `
+      You can use <b>bold text</b>,
+      <a href="#" autofocus>links</a>,
+      and other HTML tags
+    `,
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfirm: false,
+    confirmButtonText: `
+      <i class="fa fa-thumbs-up"></i> Great!
+    `,
+    confirmButtonAriaLabel: "Thumbs up, great!",
+    cancelButtonText: `
+      <i class="fa fa-thumbs-down"></i>
+    `,
+    cancelButtonAriaLabel: "Thumbs down",
+  });
   return (
     <div>
       <img className="w-80 h-80" src={image.src} alt={image.alt} />
